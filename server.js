45a984f3fcebc09cf598cb1b1c9d5ae1dc9a699f
Samuel Daley <106825127+Soreid@users.noreply.github.com>
2025-09-09 13:45:26 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,78 @@
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+import express from 'express';
+import { Resend } from 'resend';
+import dotenv from 'dotenv';
+
+const secret_name = "Portfolio_Email";
+
+const client = new SecretsManagerClient({
+  region: "us-east-2",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const api_key = response.SecretString.Resend_API;
+
+console.log(api_key);
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mailer = new Resend(api_key);
+
+app.use(express.json());
+app.use(express.static('public'));
+
+async function sendMail (name, email, message) {
+  const { data, error } = await mailer.emails.send({
+    from: 'Samuel <samuel@samueldaley.com>',
+    to: ['samuel@samueldaley.com'],
+    subject: 'New Website Message',
+    html: `Hello! You have received a new message from ${name} at ${email}:<br><br>${message}`
+  });
+
+  if (error) {
+    return console.error({ error });
+  }
+
+  console.log({ data });
+  return 200;
+};
+
+app.post('/api/send', async (req, res) => {
+  mailDetails = req.body;
+
+  mailingStatus = await sendMail(mailDetails.name, mailDetails.email, mailDetails.message);
+
+  if(mailingStatus === 200) {
+    res.status(200).send();
+  } else {
+    res.status(400).send(mailingStatus);
+  }
+});
+
+app.get('/api/status', (req, res) => {
+  res.status(200).send("The server is currently operational.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
